ff961e98567769ddc9dffb7630a391595adf91a3
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-01-21 17:36:11 +0100

    Add jumbotron and logo header

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -1,3 +1,16 @@
 <header>
-    <h2>header</h2>
+    
+    <img src="{{asset('/img/dc-logo.png')}}" alt="logo">
+    <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+    </ul>
 </header>
