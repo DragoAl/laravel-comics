c5404a6f2df022e4fe36d2cda1b93e39223b0bc9
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-01-22 18:45:09 +0100

    Add  buy-section in products

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -7,14 +7,42 @@
         <div class="comics-container">
             @foreach ($products as $item)
                 <div class="comics">
-                    <img src="{{$item['thumb']}}" alt="Comics">
-                    <h4>"{{$item['series']}}"</h4>
+                    <a href="{{route('comicBook')}}">
+                        <img src="{{$item['thumb']}}" alt="Comics">
+                        <h4>"{{$item['series']}}"</h4>
+                    </a>
+                    
                 </div>
             @endforeach
         </div>
 
         <div class="btn-style btn-load">LOAD MORE</div>
 
+    </section>
+    <section class="section-buy">
+        <ul>
+            <li>
+                <img src="{{asset('/img/buy-comics-digital-comics.png')}}"  alt="info.text">  
+                <a href="#"> DIGITAL COMICS</a>
+            </li>
+            <li>
+                <img src="{{asset('/img/buy-comics-merchandise.png')}}"  alt="info.text">  
+                <a href="#"> DC MERCHANDISE</a>
+            </li>
+            <li>
+                <img src="{{asset('/img/buy-comics-subscriptions.png')}}"  alt="info.text">  
+                <a href="#"> SUBSCRIPTION</a>
+            </li>
+            <li>
+                <img src="{{asset('/img/buy-comics-shop-locator.png')}}"  alt="info.text">  
+                <a href="#"> COMIC SHOP LOCATOR</a>
+            </li>
+            <li>
+                <img src="{{asset('/img/buy-dc-power-visa.svg')}}"  alt="info.text">  
+                <a href="#"> DC POWER VISA</a>
+            </li>
+        </ul>
+    
     </section>
     
 @endsection
