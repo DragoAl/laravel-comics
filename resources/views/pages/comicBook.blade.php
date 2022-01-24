f3f864881cbc2f894252a3811aaf2a77243f2686
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-01-24 01:26:32 +0100

    Add base layout

--- resources/views/pages/comicBook.blade.php
+++ resources/views/pages/comicBook.blade.php
@@ -1,7 +1,53 @@
 @extends('layout.main-layout')
 @section('content')
+    <div class="blue-bar">
+        <img src="" alt="comicBook">
+    </div>
+    <section class="container-comicBook-adv">
+        <div class="container-info-comic">
+            <h3 class="comicBook-title"> Action Comic book</h3>
+            <div class="green-infobar"></div>
+            <p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus.</p>
+        </div>
+        
+        <div class="advertising">
+            <div class="advertising-banner">
 
-    <section>
+            </div>
+        </div>
+
+    </section>
+    <section class="info-comic ">
+        <section>
+            <h3>Talent</h3>
+            <div>
+                <h6>Art By:</h6>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet explicabo hic eos nobis cum quibusdam porro illo? Libero illo facilis enim. Ratione enim id, vitae cum eos aperiam tempore quis?</span>
+            </div>
+            <div>
+                <h6>Written By:</h6>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet explicabo hic eos nobis cum quibusdam porro illo? Libero illo facilis enim. Ratione enim id, vitae cum eos aperiam tempore quis?</span>
+            </div>
+           
+        </section>
+        <section>
+            <h3>Specs</h3>
+            <div>
+                <h6>Series:</h6>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                </span>
+            </div>
+            <div>
+                <h6>U.S. Price</h6>
+                <span>$20 </span>
+            </div>
+            <div>
+                <h6>On Sale Date:</h6>
+                <span>october 2004 </span>
+            </div>
+           
+        </section>
+        
 
     </section>
 @endsection
